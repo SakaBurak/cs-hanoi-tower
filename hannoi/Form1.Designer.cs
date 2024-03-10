4dddab87b4b4f23feebd5562e915f86e181fdd95
SakaBurak <bekirburaksaka@gmail.com>
2024-03-10 15:49:22 +0300

    Proje dosyası ekle.

---
+++ hannoi/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace hannoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(14, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Oynat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(293, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(349, 84);
+            this.listBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(650, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(95, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(65, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Disk Sayısı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.label2.Location = new System.Drawing.Point(287, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 34);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "(2^n)-1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(46, 412);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "1.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(293, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "2.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(530, 411);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "3.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(679, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hanoi Kuleleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ hannoi/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace hannoi
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * 221030910008
+         * Bekir Burak SAKA
+         * 
+         * Zaman Kompleksi : O(2^n)
+         * 
+         * Kaba Kod:
+         * 
+         *   //çubukları oluştur
+         *   //diskleri oluştur
+         *   //oluşturduğun diskin büyüklüğünü kontrol et
+         *   //sayaç ata (hamle sayısı için)
+         *   //diskleri hareket ettir DiskiTaşı();
+         *   //ilk başta kaç disk kaldığını kontrol et     if (diskSayısı == 1){DiskiTaşı(çubuklar, kaynak, hedef);}
+         *   //tek disk kalana kadar tekrar et
+         *   else{
+                HanoiÇözümü(diskSayısı - 1, kaynak, hedef, ara);
+                DiskiTaşı(çubuklar, kaynak, hedef);
+                HanoiÇözümü(diskSayısı - 1, ara, kaynak, hedef);
+            }
+         */
+
+        private const int ÇubukSayısı = 3; //Hanoi Kulelerimiz
+
+        private List<Label>[] çubuklar;
+        private int hamleSayısı;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ÇubuklarıHazırla();
+
+            // ComboBox'a eleman ekle
+            for (int i = 3; i <= 9; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+            comboBox1.SelectedIndex = 0; // Varsayılan olarak 3 disk seç
+        }
+
+        private void ÇubuklarıHazırla()
+        {
+            // Seçilen disk sayısına göre çubukları hazırla
+            int diskSayısı = Convert.ToInt32(comboBox1.SelectedItem);
+            çubuklar = new List<Label>[ÇubukSayısı];
+            for (int i = 0; i < ÇubukSayısı; i++)
+            {
+                çubuklar[i] = new List<Label>();
+            }
+
+            // İlk olarak ilk çubuğa diskleri ekle
+            for (int i = diskSayısı; i >= 1; i--)
+            {
+                Label diskEtiketi = new Label();
+                diskEtiketi.Text = "Disk" + i;
+                diskEtiketi.TextAlign = ContentAlignment.MiddleCenter;
+                diskEtiketi.AutoSize = false;
+                diskEtiketi.Size = new Size(80 + i * 20, 20);
+                diskEtiketi.BackColor = Color.PowderBlue; // Buradan başlangıç disk renklerini değiştirebilirim
+                çubuklar[0].Add(diskEtiketi);
+            }
+
+            // Diskleri konumlandır
+            DiskleriKonumlandır();
+        }
+
+        private void DiskleriKonumlandır()
+        {
+            const int başlangıçX = 50;
+            const int çubukBoşluğu = 175;
+
+            for (int çubukİndex = 0; çubukİndex < ÇubukSayısı; çubukİndex++)
+            {
+                int x = başlangıçX + çubukİndex * çubukBoşluğu;
+                int y = 300;
+
+                for (int i = 0; i < çubuklar[çubukİndex].Count; i++)
+                {
+                    Label diskEtiketi = çubuklar[çubukİndex][i];
+                    diskEtiketi.Location = new Point(x - diskEtiketi.Width / 2, y);
+                    y -= diskEtiketi.Height + 5;
+                    Controls.Add(diskEtiketi);
+                }
+            }
+        }
+
+        private void DiskiTaşı(List<Label>[] çubuklar, int kaynak, int hedef)
+        {
+            if (çubuklar[kaynak].Count == 0)
+                return;
+
+            Label disk = çubuklar[kaynak][çubuklar[kaynak].Count - 1];
+            çubuklar[kaynak].Remove(disk);
+            çubuklar[hedef].Add(disk);
+
+            DiskleriKonumlandır();
+            hamleSayısı++;
+            listBox1.Items.Add("Hamle " + hamleSayısı + ": Diski " + (kaynak + 1) + ". Çubuktan " + (hedef + 1) + ". Çubuğa taşı");
+            disk.BackColor = Color.Gainsboro;  //oynanan disklerin rengini değiştir.
+
+            System.Threading.Thread.Sleep(1000); // 1sn bekleyerek yap.
+        }
+
+       
+
+
+
+        private void HanoiÇözümü(int diskSayısı, int kaynak, int ara, int hedef)
+        {
+            if (diskSayısı == 1)
+            {
+                DiskiTaşı(çubuklar, kaynak, hedef);
+            }
+            else
+            {
+                HanoiÇözümü(diskSayısı - 1, kaynak, hedef, ara);
+                DiskiTaşı(çubuklar, kaynak, hedef);
+                HanoiÇözümü(diskSayısı - 1, ara, kaynak, hedef);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("! Hanoi Kuleleri !");
+            MessageBox.Show("1-Her Hamlede bir disk hareket ettirilebilir                                             " +
+                "2-Bir disk kendisinden küçük bir diskin üzerine yerleştirilemez", "Oyun Kuralları");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            hamleSayısı = 0;
+            ÇubuklarıHazırla(); // Disk sayısı combobox'tan değiştirildiyse tekrar hazırla
+            Task.Run(() => HanoiÇözümü(Convert.ToInt32(comboBox1.SelectedItem), 0, 1, 2));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
